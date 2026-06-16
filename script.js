4e1fd54d2b4a81d3d454e8024dac1b5e7151ea5b
prabhat <kapkotiprabhat@gmail.com>
2026-06-16 22:18:41 +0530

    Used Geolocation to you get you current coordinates

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+    const { latitude, longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${29.9660219},${79.9077507}`);
+  },
+  function () {
+    alert('Could not get your position');
+  },
+  {
+    enableHighAccuracy: true,
+  },
+);
